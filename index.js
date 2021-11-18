d20393613c48cd100a94c5772736e5172ebd20b7
AZIZXlaouiti <azizrocke@gmail.com>
2021-11-18 12:16:24 -0500

    getting the scraper working

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const axios = require('axios')
+const axios = require('axios');
+const cheerio  = require('cheerio');
 const url = "https://github.com/AZIZXlaouiti"
-const foo = "https://api.github.com/users/AZIZXlaouiti"
+// $('#user-79036942-pinned-items-reorder-form > ol')
+// const foo = "https://api.github.com/users/AZIZXlaouiti"
 const getData = async()=>{
   const { data } =  await axios.get(url);
-  const {data:{id}}  = await axios.get(foo)
-  console.log("id",id)
-  const table = $(`#user-${id}-pinned-items-reorder-form > ol`)
+  const $ = cheerio.load(data)
+//   const {data:{id}}  = await axios.get(foo)
+  const table = $('.js-pinned-items-reorder-list')
+   table.find('ol li').each((i, e)=>{
+       const $elemet = $(e)
+       console.log($elemet.text())
+   })
 }
 getData();
