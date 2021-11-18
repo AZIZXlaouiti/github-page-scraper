acb997ee78aed914a2769b0390334bb8b7f9f9a3
AZIZXlaouiti <azizrocke@gmail.com>
2021-11-18 15:53:00 -0500

    getting there

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const axios = require('axios');
 const cheerio  = require('cheerio');
 const url = "https://github.com/AZIZXlaouiti"
+
+const form = []
 // $('#user-79036942-pinned-items-reorder-form > ol')
 // const foo = "https://api.github.com/users/AZIZXlaouiti"
 const getData = async()=>{
   const { data } =  await axios.get(url);
   const $ = cheerio.load(data)
-//   const {data:{id}}  = await axios.get(foo)
+  //   const {data:{id}}  = await axios.get(foo)
   const table = $('.js-pinned-items-reorder-list')
-   table.find('ol li').each((i, e)=>{
-       const $elemet = $(e)
-       console.log($elemet.text())
-   })
+  table.find('ol > li').each((i, e)=>{
+      const $elemet = $(e)
+      const state = {}
+      state.name = $($elemet.find('span').first()).text()
+      form.push(state)
+      //getting pined repos
+    })
+    // console.log(form)
+  form.forEach(async(e)=>{
+      const baz = `${url}/${e.name}`
+      const { data } =  await axios.get(baz);
+      const $ = cheerio.load(data)
+      const table = $('#readme > div> article:contains("Technologies") ')
+      table.find('ul>li').each((i, e)=>{
+        const $elemet = $(e)
+        console.log($elemet.text())
+        //getting pined repos
+      })
+  })  
 }
 getData();
