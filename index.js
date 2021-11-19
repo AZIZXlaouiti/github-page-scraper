b72ec0d259be875133b4d1b1b54a2cac211f0a50
AZIZXlaouiti <azizrocke@gmail.com>
2021-11-18 20:34:18 -0500

    making scraping more optimized

--- index.js
+++ index.js
@@ -1,41 +1,60 @@
 const axios = require('axios');
 const cheerio  = require('cheerio');
 const url = "https://github.com/AZIZXlaouiti"
+const form = []
 
 
 // $('#user-79036942-pinned-items-reorder-form > ol')
 // const foo = "https://api.github.com/users/AZIZXlaouiti"
 const getData = async()=>{
-    const form = []
-    const lan = []
     const { data } =  await axios.get(url);
     const $ = cheerio.load(data)
     //   const {data:{id}}  = await axios.get(foo)
     const table = $('.js-pinned-items-reorder-list')
-    table.find('ol > li').each((i, e)=>{
+    table.find('ol > li').each(async(i, e)=>{
         const $elemet = $(e)
         const state = {}
         state.name = $($elemet.find('span').first()).text()
-        form.push(state)
-        //getting pined repos
-    })
-    form.forEach(async(e)=>{
-        const baz = `${url}/${e.name}`
-        const { data } =  await axios.get(baz);
-        const $ = cheerio.load(data)
-        const table = $('#readme > div > article:contains("Technologies") ')
-        if (table.length === 0){
-            return
-        }
-        table.find('ul').first().each((i, e)=>{
+        const foo = `${url}/${state.name}`
+        const { data } =  await axios.get(foo);
+        const $row = cheerio.load(data)
+        const row = $row('#readme > div > article:contains("Technologies") ')
+        if (row.length === 0){
+            form.push(state)
+            console.log(state)
+            
+                }else {
+
+        row.find('ul').first().each((i, e)=>{
             const $elemet = $(e)
-            const bar = {}
-            bar.technologie  = $elemet.text().trim().split('\n')
-            form.push(bar)
-            // console.log(bar)
-            //getting pined repos
+            state.technologie  = $elemet.text().trim().split('\n')
+            console.log(state)
+                    })        
+                
+                    form.push(state)
+                }
         })
-    })  
-    console.log(form)
+        
+        // console.log(form)
+ 
 }
 getData();
+
+// -------------------------------------------------------------------------- //
+    // form.forEach(async(e)=>{
+    //     const baz = `${url}/${e.name}`
+    //     const { data } =  await axios.get(baz);
+    //     const $ = cheerio.load(data)
+    //     const table = $('#readme > div > article:contains("Technologies") ')
+    //     if (table.length === 0){
+    //         return
+    //     }
+    //     table.find('ul').first().each((i, e)=>{
+    //         const $elemet = $(e)
+    //         const bar = {}
+    //         bar.technologie  = $elemet.text().trim().split('\n')
+    //         form.push(bar)
+    //         // console.log(form)
+    //         //getting pined repos
+    //     })
+    // }) 
